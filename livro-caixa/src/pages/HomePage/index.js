19066fc71070ce49ab81a66b08541c41e0644c2d
Raphael Remião <raphael.remiao@edu.pucrs.br>
2020-05-22 22:24:17 -0300

    feat: finalizing user show and change

--- livro-caixa/src/pages/HomePage/index.js
+++ livro-caixa/src/pages/HomePage/index.js
@@ -11,10 +11,22 @@ class HomePage extends Component {
   render() {
     return (
       <Container>
-        <Grid style={{display:"flex", flexDirection:"row", justifyContent:"flex-start"}}>
-          <text
-            
-          > O seu usuário atual é: {this.state.user}, para trocá-lo <Button onClick={() => window.location.href="/user"}>clique aqui</Button></text>
+        <Grid style={{display:"flex", flexDirection:"column", justifyContent:"center", marginTop:20}}>
+        <Grid item md={3} style={{display:"flex", flexDirection:"column", justifyContent:"center"}}>
+          <text>O seu usuário atual é {this.state.user}, para troca-lo utilize os botões abaixo!</text>
+          <Button onClick={() => this.setState({user:"User 1"})}>
+              Usuário 1
+          </Button>
+          <Button onClick={() =>  this.setState({user:"User 2"})}>
+              Usuário 2
+          </Button>
+          <Button onClick={() =>  this.setState({user:"User 3"})}>
+              Usuário 3
+          </Button>
+          <Button onClick={() =>  this.setState({user:"User 4"})}>
+              Usuário 4
+          </Button>
+          </Grid>
         </Grid>
       </Container>
     );
